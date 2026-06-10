116ebd2ae4d2ae8560ef513d0cb4990728e47884
massivelowtaperfade0 <massivelowtaperfade0@gmail.com>
2026-06-10 16:00:29 +0530

    feat: backend & db deployed and working

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello there, and welcome to schedula-bhavesh, an Doctor Appointment Booking API and server, this is for demonstration purposes only, do not mistaken it for actual service';
   }
 }
